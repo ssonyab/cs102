5d198c9b9af32c53af345db7ca3ff8208a1100d6
sofia <sofya_bikova17@mail.ru>
2020-10-21 23:52:29 +0300

    I did caesar

--- homework01/caesar.py
+++ homework01/caesar.py
@@ -15,32 +15,72 @@ def encrypt_caesar(plaintext: str, shift: int = 3) -> str:
     ''
     """
     ciphertext = ""
-    # PUT YOUR CODE HERE
+    text = list(plaintext)
+    for i in text:
+        i = ord(i)
+        if i >= ord('a') and i <= ord('z') or i >= ord('A') and i <= ord('Z'):
+            if i > ord('z') - shift and i <= ord('z'):
+                i -= 26
+            elif i > ord('Z') - shift and i <= ord('Z'):
+                i -= 26
+            i += shift
+            i = chr(i)
+            ciphertext += i
+        else:
+            i = chr(i)
+            ciphertext += i
     return ciphertext
-
 
 def decrypt_caesar(ciphertext: str, shift: int = 3) -> str:
     """
-    Decrypts a ciphertext using a Caesar cipher.
+      Decrypts a ciphertext using a Caesar cipher.
 
-    >>> decrypt_caesar("SBWKRQ")
-    'PYTHON'
-    >>> decrypt_caesar("sbwkrq")
-    'python'
-    >>> decrypt_caesar("Sbwkrq3.6")
-    'Python3.6'
-    >>> decrypt_caesar("")
-    ''
-    """
+      >>> decrypt_caesar("SBWKRQ")
+      'PYTHON'
+      >>> decrypt_caesar("sbwkrq")
+      'python'
+      >>> decrypt_caesar("Sbwkrq3.6")
+      'Python3.6'
+      >>> decrypt_caesar("")
+      ''
+      """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    text = list(ciphertext)
+    for i in text:
+        i = ord(i)
+        if i >= ord('a') and i <= ord('z') or i >= ord('A') and i <= ord('Z'):
+            if i < ord('a') + shift and i >= ord('a'):
+                i += 26
+            elif i < ord('A') + shift and i >= ord('A'):
+                i += 26
+            i -= shift
+            i = chr(i)
+            plaintext += i
+        else:
+            i = chr(i)
+            plaintext += i
     return plaintext
 
-
-def caesar_breaker_brute_force(ciphertext: str, dictionary: tp.Set[str]) -> int:
+def caesar_breaker(ciphertext: str, dictionary: tp.Set[str]) -> int:
     """
-    Brute force breaking a Caesar cipher.
+           >>> d = {"python", "java", "ruby"}
+           >>> caesar_breaker("python", d)
+           0
+           >>> caesar_breaker("sbwkrq", d)
+           3
     """
     best_shift = 0
-    # PUT YOUR CODE HERE
+    text = list(ciphertext.lower())
+    for i in dictionary:
+        el = list(i)
+        sh1 = ord(text[0]) - ord(el[0])
+        sh2 = ord(text[1]) - ord(el[1])
+        if sh1 < 0:
+            sh1 += 26
+        if sh2 < 0:
+            sh2 += 26
+        if sh1 == sh2:
+            best_shift = sh1
     return best_shift
+
+
