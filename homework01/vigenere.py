2084b52e68ce5d5571d60aefeb8f11615c647616
sofia <sofya_bikova17@mail.ru>
2020-10-24 15:15:54 +0300

    Vign

--- homework01/vigenere.py
+++ homework01/vigenere.py
@@ -10,7 +10,18 @@ def encrypt_vigenere(plaintext: str, keyword: str) -> str:
      'LXFOPVEFRNHR'
      """
     ciphertext = ""
-
+    for sn in range(len(plaintext)):
+        ltr = plaintext[sn]
+        keyword = keyword.upper()
+        shift = ord(keyword[sn % len(keyword)]) - 65
+        sn = 0
+        if ltr.islower():
+            ciphertext += chr(97 + ((ord(ltr) - 97 + shift) % 26))
+        elif ltr.isupper():
+            ciphertext += chr(65 + ((ord(ltr) - 65 + shift) % 26))
+        else:
+            ciphertext += ltr
+        sn += 1
     return ciphertext
 
 
@@ -26,5 +37,16 @@ def decrypt_vigenere(ciphertext: str, keyword: str) -> str:
     'ATTACKATDAWN'
     """
     plaintext = ""
-    # PUT YOUR CODE HERE
+    for sn in range(len(ciphertext)):
+        ltr = plaintext[sn]
+        keyword = keyword.upper()
+        shift = ord(keyword[sn % len(keyword)]) - 65
+        sn = 0
+        if ltr.islower():
+            plaintext += chr(97 + ((ord(ltr) - 97 - shift) % 26))
+        elif ltr.isupper():
+            plaintext += chr(65 + ((ord(ltr) - 65 - shift) % 26))
+        else:
+            plaintext += ltr
+        sn += 1
     return plaintext
