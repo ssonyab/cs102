3a10c8f58fec9653544fd4f41cf5aca2ac3b527f
sofia <sofya_bikova17@mail.ru>
2020-11-08 22:17:14 +0300

    sudoku5.2.0

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -79,6 +79,8 @@ def get_block(grid: List[List[str]], pos: Tuple[int, int]) -> List[str]:
     >>> get_block(grid, (8, 8))
     ['2', '8', '.', '.', '.', '5', '.', '7', '9']
     """
+    row = pos[0] // 3
+    col = pos[1] // 3
     return [
         grid[3 * row + i][3 * col + j]
         for i in range(row, row + 3)
