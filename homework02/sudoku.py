923a27213f72b4f37f8f49cbf6d99546663ce44a
sofia <sofya_bikova17@mail.ru>
2020-11-08 21:50:53 +0300

    sudoku5.2.0

--- homework02/sudoku.py
+++ homework02/sudoku.py
@@ -79,25 +79,9 @@ def get_block(grid: List[List[str]], pos: Tuple[int, int]) -> List[str]:
     >>> get_block(grid, (8, 8))
     ['2', '8', '.', '.', '.', '5', '.', '7', '9']
     """
-    luk = []
-    if pos[0] < 3:
-        line_num = 0
-    elif pos[0] < 6:
-        line_num = 3
-    else:
-        line_num = 6
-    line_list = grid[line_num : line_num + 3]
-    if pos[1] < 3:
-        col_num = 0
-    elif pos[1] < 6:
-        col_num = 3
-    else:
-        col_num = 6
-    for i in range(3):
-        for m in range(col_num, col_num + 3):
-            luk.append(line_list[i][m])
-            print(i, m, len(line_list), len(line_list[i]))
-    return luk
+    row = pos[0] // 3
+    column = pos[1] // 3
+    return [grid[i][j] for i in range(row, row + 3) for j in range(column, column + 3)]
 
 
 def find_empty_positions(grid: List[List[str]]) -> Optional[Tuple[int, int]]:
