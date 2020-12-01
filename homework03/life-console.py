9755aa8a5b3fa5e731c50aa24e58a2c19a4ef7a4
sofia <sofya_bikova17@mail.ru>
2020-12-01 17:09:21 +0300

    console

--- homework03/life-console.py
+++ homework03/life-console.py
@@ -3,21 +3,52 @@ import curses
 from life import GameOfLife
 from ui import UI
 
+# pylint: disable=no-member
+# pylint: disable=missing-class-docstring
+# pylint: disable=missing-module-docstring
+# pylint: disable=missing-function-docstring
+
 
 class Console(UI):
-
     def __init__(self, life: GameOfLife) -> None:
         super().__init__(life)
+        self.rows = len(self.life.curr_generation)
+        self.cols = len(self.life.curr_generation[0])
 
     def draw_borders(self, screen) -> None:
         """ Отобразить рамку. """
-        pass
+        screen.border()
 
     def draw_grid(self, screen) -> None:
         """ Отобразить состояние клеток. """
-        pass
+        for i in range(1, self.rows - 1):
+            for m in range(1, self.cols - 1):
+                if self.life.curr_generation[i][m]:
+                    bam = "*"
+                else:
+                    bam = ""
+                screen.addch(i, m, bam)
 
     def run(self) -> None:
         screen = curses.initscr()
-        # PUT YOUR CODE HERE
-        curses.endwin()
+        sc = screen.derwin(self.rows, self.cols, 0, 0)
+        self.draw_borders(sc)
+
+        try:
+            while self.life.is_changing or not self.life.is_max_generations_exceeded:
+                self.draw_borders(sc)
+                self.draw_grid(sc)
+                sc.refresh()
+                self.life.step()
+        finally:
+            curses.endwin()
+
+
+def main():
+    game = GameOfLife(size=(32, 80))
+    app = Console(game)
+    app.run()
+
+
+if __name__ == "__main__":
+    main()
