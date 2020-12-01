3d7fb224819e74d850ed15a683a93ed41f9a823c
sofia <sofya_bikova17@mail.ru>
2020-12-01 21:36:57 +0300

    ui

--- homework03/ui.py
+++ homework03/ui.py
@@ -5,8 +5,8 @@ from life import GameOfLife
 
 class UI(abc.ABC):
 
-        def __init__(self, life: GameOfLife) -> None:
-            self.life = life
+    def __init__(self, life: GameOfLife) -> None:
+        self.life = life
 
     @abc.abstractmethod
     def run(self) -> None:
