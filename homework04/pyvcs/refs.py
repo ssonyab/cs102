da0512654fcd6e9031c14555923be14f0282ec71
sofia <sofya_bikova17@mail.ru>
2020-12-19 18:17:49 +0300

    jnj

--- homework04/pyvcs/refs.py
+++ homework04/pyvcs/refs.py
@@ -8,12 +8,12 @@ def update_ref(gitdir: pathlib.Path, ref: tp.Union[str, pathlib.Path], new_value
 
 
 def symbolic_ref(gitdir: pathlib.Path, name: str, ref: str) -> None:
-    real_file = gitdir / name
+    ref_file = gitdir / name
     with (ref_file).open("w") as file:
         file.write(ref)
 
 
-def ref_resolve(gitdir: pathlib.Path, refname: str) -> str:
+def ref_resolve(gitdir: pathlib.Path, refname: str) -> tp.Optional[str]:
     if refname == "HEAD" and not is_detached(gitdir):
         return resolve_head(gitdir)
     if (gitdir / refname).exists():
